268537790853bd9ee4866dc8486b5d9a032b004d
Eduardo Klosowski <eduardo_klosowski@yahoo.com>
2024-08-11 19:49:36 -0300

    Configura docker

--- scripts/setup-devcontainer.sh
+++ scripts/setup-devcontainer.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 set -xe
+
+# Completion
+mkdir -p ~/.local/share/bash-completion/completions
+echo 'eval "$(docker completion bash)"' > ~/.local/share/bash-completion/completions/docker
