18c525bdf6bcb6ed34f73e3e017aa396c2ee9960
Eduardo Klosowski <eduardo_klosowski@yahoo.com>
2024-08-18 16:58:38 -0300

    Cria mixin para timestamp nas tabelas

--- src/madr/models.py
+++ src/madr/models.py
@@ -1,4 +1,14 @@
-from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass
+from datetime import datetime
+
+from sqlalchemy import func
+from sqlalchemy.orm import DeclarativeBase, Mapped, MappedAsDataclass, mapped_column
+
+
+class TimestampMixin(MappedAsDataclass):
+    created_at: Mapped[datetime] = mapped_column(init=False, repr=False, server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        init=False, repr=False, server_default=func.now(), onupdate=func.now()
+    )
 
 
 class Base(DeclarativeBase, MappedAsDataclass): ...
